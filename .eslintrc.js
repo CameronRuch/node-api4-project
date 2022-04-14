e367bb3429b958d932fe7ab16331e215f7818374
CameronRuch <cameron.ruch@yahoo.com>
2022-04-14 15:52:24 -0500

    set up server

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,15 @@
 module.exports = {
     "env": {
-        "browser": true,
+        "commonjs": true,
         "es2021": true,
         "node": true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
+    "extends": "eslint:recommended",
     "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
         "ecmaVersion": "latest",
-        "sourceType": "module"
     },
-    "plugins": [
-        "react"
-    ],
     "rules": {
-        "no-unused-vars": 0,
-        "no-empty": 0,
-        
+        "no-unused-vars": 1,
+        "no-empty": 1,
     }
 }
